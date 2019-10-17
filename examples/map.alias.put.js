c476ef0ddba18ffa2ec5eaf9215abb7fa7f590f2
Trygve Lie <post@trygve-lie.com>
2019-10-17 16:50:42 +0200

    Made import maps immutable

---
+++ examples/map.alias.put.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const FormData = require('form-data');
+const fetch = require('node-fetch');
+
+const formData = new FormData();
+formData.append('version', '4.2.2');
+
+fetch('http://localhost:4001/biz/map/buzz/v4', {
+    method: 'PUT',
+    body: formData
+})
+.then((res) => res.json())
+.then(obj => console.log(obj));
---
+++ examples/map.put.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const FormData = require('form-data');
+const { Writable } = require('stream');
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+const formData = new FormData();
+formData.append('map', fs.createReadStream('../fixtures/import-map.json'));
+
+fetch('http://localhost:4001/biz/map/buzz/4.2.2', {
+    method: 'PUT',
+    body: formData
+})
+.then((res) => {
+    const stream = new Writable({
+        objectMode: false,
+        write(chunk, encoding, callback) {
+            console.log(JSON.parse(chunk.toString()));
+            callback();
+        },
+    });
+    res.body.pipe(stream);
+});
