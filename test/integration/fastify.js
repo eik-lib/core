73d03442eba1068fb45e3a3588c949f00e6943b8
Richard Walker <digitalsadhu@gmail.com>
2019-10-22 10:23:27 +1300

    add another assertion to test

--- test/integration/fastify.js
+++ test/integration/fastify.js
@@ -53,9 +53,9 @@ test('Packages PUT - all files extracted, files accessible after upload', async 
     const file2 = await fetch(
         'http://localhost:4001/foo/pkg/bar/1.1.1/main/index.js.map',
     );
-    // const file3 = await fetch(
-    //     'http://localhost:4001/foo/pkg/bar/1.1.1/ie11/index.js',
-    // );
+    const file3 = await fetch(
+        'http://localhost:4001/foo/pkg/bar/1.1.1/ie11/index.js',
+    );
     // const file4 = await fetch(
     //     'http://localhost:4001/foo/pkg/bar/1.1.1/ie11/index.js.map',
     // );
@@ -71,7 +71,7 @@ test('Packages PUT - all files extracted, files accessible after upload', async 
 
     t.equals(file1.status, 200, 'GET to index.js responded with 200 ok');
     t.equals(file2.status, 200, 'GET to index.js.map responded with 200 ok');
-    // t.equals(file3.status, 200, 'GET to ie11 index.js responded with 200 ok');
+    t.equals(file3.status, 200, 'GET to ie11 index.js responded with 200 ok');
     // t.equals(
     //     file4.status,
     //     200,
