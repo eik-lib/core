8e9199b30562878b8ab2f43d93c974be089e1380
Trygve Lie <post@trygve-lie.com>
2019-11-04 06:48:36 +0100

    Check if package exists before upload

--- examples/pkg.put.js
+++ examples/pkg.put.js
@@ -15,5 +15,34 @@ fetch('http://localhost:4001/biz/pkg/fuzz/8.4.1', {
     body: formData,
     headers: formData.getHeaders(),
 })
-.then(res => res.json())
-.then(json => console.log(json));
+.then(res => {
+    let result = {};
+    switch (res.status) {
+        case 201:
+            result = res.json();
+            break;
+        case 400:
+            result = { status: res.status, message: 'Invalid URL parameter' };
+            break;
+        case 401:
+            result = { status: res.status, message: 'Unauthorized' };
+            break;
+        case 409:
+            result = { status: res.status, message: 'Package exist' };
+            break;
+        case 415:
+            result = { status: res.status, message: 'Unsupported file format' };
+            break;
+        case 502:
+            result = { status: res.status, message: 'Writing file failed' };
+            break;
+        default:
+            result = { status: res.status };
+    }
+    return result;
+})
+.then(obj => console.log(obj))
+.catch(error => {
+    console.log(error);
+});
+
