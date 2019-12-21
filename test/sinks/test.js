a580049623fe325cd3a5072fd3b85b8636383777
Trygve Lie <post@trygve-lie.com>
2019-12-21 08:26:31 +0100

    Clean up test sink tests

--- test/sinks/test.js
+++ test/sinks/test.js
@@ -9,11 +9,8 @@ const fs = require('fs');
 
 const Sink = require('../../lib/sinks/test');
 
-
-const DEFAULT_CONFIG = {
-    // sinkFsRootPath: path.join(os.tmpdir(), '/eik-test-files')
-};
-const fixture = fs.readFileSync(path.join(__dirname, '../../fixtures/import-map.json')).toString();
+const DEFAULT_CONFIG = {};
+const FIXTURE = fs.readFileSync(path.join(__dirname, '../../fixtures/import-map.json')).toString();
 
 const readFileStream = (file = '../README.md') => {
     const pathname = path.join(__dirname, file);
@@ -105,7 +102,7 @@ test('Sink() - .read() - File exists', async (t) => {
 
     const result = await pipeInto(readFrom.stream);
 
-    t.equal(result, fixture, 'should read file from sink which equals the fixture');
+    t.equal(result, FIXTURE, 'should read file from sink which equals the fixture');
 
     // Clean up sink
     await sink.delete(dir);
