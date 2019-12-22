3885bd4fe7acde2c63ca3dca19e9f0855a44751e
Trygve Lie <173003+trygve-lie@users.noreply.github.com>
2019-12-22 22:31:12 +0100

    Adding more tests (#84)
    * Use ReadFile class from @eik/common
    
    * Clean up test sink tests
    
    * Added tests for Asset class
    
    * Test Asset class better

--- test/classes/package.js
+++ test/classes/package.js
@@ -1,13 +1,9 @@
 'use strict';
 
-const tap = require('tap');
+const { test } = require('tap');
 const Package = require('../../lib/classes/package');
 
-//
-// Constructor
-//
-
-tap.test('Package() - object type - should be Package', (t) => {
+test('Package() - object type - should be Package', (t) => {
     const pkg = new Package();
     t.equal(Object.prototype.toString.call(pkg), '[object Package]');
     t.end();
