ce2d62a757334ddc858cff64d938ef9bc5aa3ca1
Trygve Lie <post@trygve-lie.com>
2020-05-27 00:22:01 +0200

    Use common multipart parser

---
+++ tap-snapshots/test-multipart-parser.js-TAP.test.js
@@ -0,0 +1,299 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+
+'use strict'
+
+exports[`test/multipart/parser.js TAP Parser() - Request contains multiple files and fields > parsed request should match snapshot 1`] = `
+Array [
+  FormFile {
+    "_name": "tgz",
+    "_value": Array [
+      Asset {
+        "_integrity": "sha512-i1oU4MCMKNS1akPh2DGG4w4SKDxj0mg7XW7jvIGx+/6wRxWREm5CCLhugpsnzc8MvhBW1h/7AT3nINtGT++nTQ==",
+        "_mimeType": "application/javascript",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/ie11/index.js",
+        "_size": 111,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-aBcMYl0ZnkXf/9yCbnVXted2i0TcwBwNZZ9zdAip7iqSbhnRAHEn0Qgbo8VrhJs6o+iiWeNzYqaqphX8J2pgEg==",
+        "_mimeType": "application/json",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/ie11/index.js.map",
+        "_size": 421,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-lq9ovpg4+LD6vF+yOFaZTnuT43fVPmCDg9FjFKl4TM3/UI8TrXeQa3WlpkoFB1WoucVwQcP9pFvZrBjtts4uHA==",
+        "_mimeType": "application/javascript",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/main/index.js",
+        "_size": 83,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-mqoo6LQtopSBDKC1DMHrilgoH+0SVdQhurrjP/MqISCxelr5PJZtT3ImEB2V7ww33ijj1NgKvdXOYnqDwFMwjw==",
+        "_mimeType": "application/json",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/main/index.js.map",
+        "_size": 425,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-sh4jQ4dz2PoNhndQlnC1XULBVkHYgjacQpnicDobIsolYbgKNybvlmZdA7oJ/66yecjAfv64i0ypAkL5nyUwbw==",
+        "_mimeType": "application/json",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/main/index.css.map",
+        "_size": 163,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-Qqu5xjdYNzRT24ox2eBngOcqmhd1dzkD21IVpBLqE/fF9uVPRHMBzqZ9lzBKosj5wE5ExOdTXSi8AWfbdAoIGA==",
+        "_mimeType": "text/css",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/main/index.css",
+        "_size": 65,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+    ],
+  },
+  FormField {
+    "_name": "foo",
+    "_value": "value-foo",
+  },
+  FormField {
+    "_name": "bar",
+    "_value": "value-bar",
+  },
+  FormFile {
+    "_name": "tar",
+    "_value": Array [
+      Asset {
+        "_integrity": "sha512-i1oU4MCMKNS1akPh2DGG4w4SKDxj0mg7XW7jvIGx+/6wRxWREm5CCLhugpsnzc8MvhBW1h/7AT3nINtGT++nTQ==",
+        "_mimeType": "application/javascript",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/ie11/index.js",
+        "_size": 111,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-aBcMYl0ZnkXf/9yCbnVXted2i0TcwBwNZZ9zdAip7iqSbhnRAHEn0Qgbo8VrhJs6o+iiWeNzYqaqphX8J2pgEg==",
+        "_mimeType": "application/json",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/ie11/index.js.map",
+        "_size": 421,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-lq9ovpg4+LD6vF+yOFaZTnuT43fVPmCDg9FjFKl4TM3/UI8TrXeQa3WlpkoFB1WoucVwQcP9pFvZrBjtts4uHA==",
+        "_mimeType": "application/javascript",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/main/index.js",
+        "_size": 83,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-mqoo6LQtopSBDKC1DMHrilgoH+0SVdQhurrjP/MqISCxelr5PJZtT3ImEB2V7ww33ijj1NgKvdXOYnqDwFMwjw==",
+        "_mimeType": "application/json",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/main/index.js.map",
+        "_size": 425,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-sh4jQ4dz2PoNhndQlnC1XULBVkHYgjacQpnicDobIsolYbgKNybvlmZdA7oJ/66yecjAfv64i0ypAkL5nyUwbw==",
+        "_mimeType": "application/json",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/main/index.css.map",
+        "_size": 163,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-Qqu5xjdYNzRT24ox2eBngOcqmhd1dzkD21IVpBLqE/fF9uVPRHMBzqZ9lzBKosj5wE5ExOdTXSi8AWfbdAoIGA==",
+        "_mimeType": "text/css",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/main/index.css",
+        "_size": 65,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+    ],
+  },
+]
+`
+
+exports[`test/multipart/parser.js TAP Parser() - Request contains only fields > parsed request should match snapshot 1`] = `
+Array [
+  FormField {
+    "_name": "foo",
+    "_value": "value-foo",
+  },
+  FormField {
+    "_name": "bar",
+    "_value": "value-bar",
+  },
+]
+`
+
+exports[`test/multipart/parser.js TAP Parser() - Request contains only files > parsed request should match snapshot 1`] = `
+Array [
+  FormFile {
+    "_name": "tgz",
+    "_value": Array [
+      Asset {
+        "_integrity": "sha512-i1oU4MCMKNS1akPh2DGG4w4SKDxj0mg7XW7jvIGx+/6wRxWREm5CCLhugpsnzc8MvhBW1h/7AT3nINtGT++nTQ==",
+        "_mimeType": "application/javascript",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/ie11/index.js",
+        "_size": 111,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-aBcMYl0ZnkXf/9yCbnVXted2i0TcwBwNZZ9zdAip7iqSbhnRAHEn0Qgbo8VrhJs6o+iiWeNzYqaqphX8J2pgEg==",
+        "_mimeType": "application/json",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/ie11/index.js.map",
+        "_size": 421,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-lq9ovpg4+LD6vF+yOFaZTnuT43fVPmCDg9FjFKl4TM3/UI8TrXeQa3WlpkoFB1WoucVwQcP9pFvZrBjtts4uHA==",
+        "_mimeType": "application/javascript",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/main/index.js",
+        "_size": 83,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-mqoo6LQtopSBDKC1DMHrilgoH+0SVdQhurrjP/MqISCxelr5PJZtT3ImEB2V7ww33ijj1NgKvdXOYnqDwFMwjw==",
+        "_mimeType": "application/json",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/main/index.js.map",
+        "_size": 425,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-sh4jQ4dz2PoNhndQlnC1XULBVkHYgjacQpnicDobIsolYbgKNybvlmZdA7oJ/66yecjAfv64i0ypAkL5nyUwbw==",
+        "_mimeType": "application/json",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/main/index.css.map",
+        "_size": 163,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-Qqu5xjdYNzRT24ox2eBngOcqmhd1dzkD21IVpBLqE/fF9uVPRHMBzqZ9lzBKosj5wE5ExOdTXSi8AWfbdAoIGA==",
+        "_mimeType": "text/css",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/main/index.css",
+        "_size": 65,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+    ],
+  },
+  FormFile {
+    "_name": "tar",
+    "_value": Array [
+      Asset {
+        "_integrity": "sha512-i1oU4MCMKNS1akPh2DGG4w4SKDxj0mg7XW7jvIGx+/6wRxWREm5CCLhugpsnzc8MvhBW1h/7AT3nINtGT++nTQ==",
+        "_mimeType": "application/javascript",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/ie11/index.js",
+        "_size": 111,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-aBcMYl0ZnkXf/9yCbnVXted2i0TcwBwNZZ9zdAip7iqSbhnRAHEn0Qgbo8VrhJs6o+iiWeNzYqaqphX8J2pgEg==",
+        "_mimeType": "application/json",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/ie11/index.js.map",
+        "_size": 421,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-lq9ovpg4+LD6vF+yOFaZTnuT43fVPmCDg9FjFKl4TM3/UI8TrXeQa3WlpkoFB1WoucVwQcP9pFvZrBjtts4uHA==",
+        "_mimeType": "application/javascript",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/main/index.js",
+        "_size": 83,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-mqoo6LQtopSBDKC1DMHrilgoH+0SVdQhurrjP/MqISCxelr5PJZtT3ImEB2V7ww33ijj1NgKvdXOYnqDwFMwjw==",
+        "_mimeType": "application/json",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/main/index.js.map",
+        "_size": 425,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-sh4jQ4dz2PoNhndQlnC1XULBVkHYgjacQpnicDobIsolYbgKNybvlmZdA7oJ/66yecjAfv64i0ypAkL5nyUwbw==",
+        "_mimeType": "application/json",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/main/index.css.map",
+        "_size": 163,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+      Asset {
+        "_integrity": "sha512-Qqu5xjdYNzRT24ox2eBngOcqmhd1dzkD21IVpBLqE/fF9uVPRHMBzqZ9lzBKosj5wE5ExOdTXSi8AWfbdAoIGA==",
+        "_mimeType": "text/css",
+        "_name": "buz",
+        "_org": "biz",
+        "_pathname": "/main/index.css",
+        "_size": 65,
+        "_type": "pkg",
+        "_version": "1.1.1",
+      },
+    ],
+  },
+]
+`
---
+++ test/multipart/parser.js
@@ -0,0 +1,250 @@
+'use strict';
+
+const { PassThrough } = require('stream');
+const FormData = require('form-data');
+const HttpError = require('http-errors');
+const path = require('path');
+const tap = require('tap');
+const fs = require('fs');
+
+const MultipartParser = require('../../lib/multipart/parser');
+const HttpIncoming = require('../../lib/classes/http-incoming');
+const Sink = require('../../lib/sinks/mem');
+
+const FIXTURE_TAR = path.resolve(__dirname, '../../fixtures/package.tar');
+const FIXTURE_BZ2 = path.resolve(__dirname, '../../fixtures/package.tar.bz2');
+const FIXTURE_GZ = path.resolve(__dirname, '../../fixtures/package.tar.gz');
+const FIXTURE_PKG = path.resolve(__dirname, '../../fixtures/archive.tgz');
+
+const Request = class Request extends PassThrough {
+    constructor ({
+        headers = {}
+    } = {}) {
+        super();
+        this.headers = {host: 'localhost', ...headers};
+    }
+}
+
+tap.test('Parser() - Object type', (t) => {
+    const obj = new MultipartParser();
+    t.equal(Object.prototype.toString.call(obj), '[object MultipartParser]', 'should be MultipartParser');
+    t.end();
+});
+
+tap.test('Parser() - Request contains multiple files and fields', async (t) => {
+    const multipart = new MultipartParser({
+        legalFields: ['foo', 'bar'],
+        legalFiles: ['tgz', 'tar'],
+        sink: new Sink(),
+    });
+
+    const formData = new FormData();
+    formData.append('tgz', fs.createReadStream(FIXTURE_GZ));
+    formData.append('foo', 'value-foo');
+    formData.append('bar', 'value-bar');
+    formData.append('tar', fs.createReadStream(FIXTURE_TAR));
+
+    const headers = formData.getHeaders();
+    const req = new Request({ headers });
+    const incoming = new HttpIncoming(req, {
+        version: '1.1.1',
+        author: {},
+        type: 'pkg',
+        name: 'buz',
+        org: 'biz',
+    });
+
+    formData.pipe(req);
+
+    const result = await multipart.parse(incoming);
+
+    t.matchSnapshot(result, 'parsed request should match snapshot');
+    t.end();
+});
+
+tap.test('Parser() - Request contains only files', async (t) => {
+    const multipart = new MultipartParser({
+        legalFiles: ['tgz', 'tar'],
+        sink: new Sink(),
+    });
+
+    const formData = new FormData();
+    formData.append('tgz', fs.createReadStream(FIXTURE_GZ));
+    formData.append('tar', fs.createReadStream(FIXTURE_TAR));
+
+    const headers = formData.getHeaders();
+    const req = new Request({ headers });
+    const incoming = new HttpIncoming(req, {
+        version: '1.1.1',
+        author: {},
+        type: 'pkg',
+        name: 'buz',
+        org: 'biz',
+    });
+
+    formData.pipe(req);
+
+    const result = await multipart.parse(incoming);
+
+    t.matchSnapshot(result, 'parsed request should match snapshot');
+    t.end();
+});
+
+tap.test('Parser() - Request contains only fields', async (t) => {
+    const multipart = new MultipartParser({
+        legalFields: ['foo', 'bar'],
+        sink: new Sink(),
+    });
+
+    const formData = new FormData();
+    formData.append('foo', 'value-foo');
+    formData.append('bar', 'value-bar');
+
+    const headers = formData.getHeaders();
+    const req = new Request({ headers });
+    const incoming = new HttpIncoming(req, {
+        version: '1.1.1',
+        author: {},
+        type: 'pkg',
+        name: 'buz',
+        org: 'biz',
+    });
+
+    formData.pipe(req);
+
+    const result = await multipart.parse(incoming);
+
+    t.matchSnapshot(result, 'parsed request should match snapshot');
+    t.end();
+});
+
+tap.test('Parser() - Request is empty', async (t) => {
+    const multipart = new MultipartParser({
+        legalFields: ['foo', 'bar'],
+        legalFiles: ['tgz', 'tar'],
+        sink: new Sink(),
+    });
+
+    const formData = new FormData();
+
+    const headers = formData.getHeaders();
+    const req = new Request({ headers });
+    const incoming = new HttpIncoming(req, {
+        version: '1.1.1',
+        author: {},
+        type: 'pkg',
+        name: 'buz',
+        org: 'biz',
+    });
+
+    formData.pipe(req);
+
+    const result = await multipart.parse(incoming);
+
+    t.same(result, [], 'should return an empty Array')
+    t.end();
+});
+
+tap.test('Parser() - Request contain illegal field name', async (t) => {
+    const multipart = new MultipartParser({
+        legalFields: ['foo', 'bar'],
+        sink: new Sink(),
+    });
+
+    const formData = new FormData();
+    formData.append('foo', 'value-foo');
+    formData.append('xyz', 'value-xyz');
+
+    const headers = formData.getHeaders();
+    const req = new Request({ headers });
+    const incoming = new HttpIncoming(req, {
+        version: '1.1.1',
+        author: {},
+        type: 'pkg',
+        name: 'buz',
+        org: 'biz',
+    });
+
+    formData.pipe(req);
+
+    t.rejects(multipart.parse(incoming), new HttpError.BadRequest(), 'should reject with bad request error');
+    t.end();
+});
+
+tap.test('Parser() - Request contain illegal field name', async (t) => {
+    const multipart = new MultipartParser({
+        legalFiles: ['tgz', 'tar'],
+        sink: new Sink(),
+    });
+
+    const formData = new FormData();
+    formData.append('tgz', fs.createReadStream(FIXTURE_GZ));
+    formData.append('xyz', fs.createReadStream(FIXTURE_TAR));
+
+    const headers = formData.getHeaders();
+    const req = new Request({ headers });
+    const incoming = new HttpIncoming(req, {
+        version: '1.1.1',
+        author: {},
+        type: 'pkg',
+        name: 'buz',
+        org: 'biz',
+    });
+
+    formData.pipe(req);
+
+    t.rejects(multipart.parse(incoming), new HttpError.BadRequest(), 'should reject with bad request error');
+    t.end();
+});
+
+tap.test('Parser() - Request contain unprocessable file', async (t) => {
+    const multipart = new MultipartParser({
+        legalFiles: ['file'],
+        sink: new Sink(),
+    });
+
+    const formData = new FormData();
+    formData.append('file', fs.createReadStream(FIXTURE_BZ2));
+
+    const headers = formData.getHeaders();
+    const req = new Request({ headers });
+    const incoming = new HttpIncoming(req, {
+        version: '1.1.1',
+        author: {},
+        type: 'pkg',
+        name: 'buz',
+        org: 'biz',
+    });
+
+    formData.pipe(req);
+
+    t.rejects(multipart.parse(incoming), new HttpError.UnprocessableEntity(), 'should reject with unprocessable entity error');
+    t.end();
+});
+
+tap.test('Parser() - Request contain file which is too large', async (t) => {
+    const multipart = new MultipartParser({
+        pkgMaxFileSize: 1024,
+        legalFiles: ['large', 'small'],
+        sink: new Sink(),
+    });
+
+    const formData = new FormData();
+    formData.append('small', fs.createReadStream(FIXTURE_GZ));
+    formData.append('large', fs.createReadStream(FIXTURE_PKG));
+
+    const headers = formData.getHeaders();
+    const req = new Request({ headers });
+    const incoming = new HttpIncoming(req, {
+        version: '1.1.1',
+        author: {},
+        type: 'pkg',
+        name: 'buz',
+        org: 'biz',
+    });
+
+    formData.pipe(req);
+
+    t.rejects(multipart.parse(incoming), new HttpError.PayloadTooLarge(), 'should reject with payload too large error');
+    t.end();
+});
