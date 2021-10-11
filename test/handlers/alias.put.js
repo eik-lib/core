d1f69e0b3d1eecba704afdd948999c29a7f63960
Martin Folkeseth <martin.folkeseth@gmail.com>
2021-10-11 22:24:49 +0200

    Check for existing import-map and npm package before POSTing og PUTing alias

--- test/handlers/alias.put.js
+++ test/handlers/alias.put.js
@@ -37,7 +37,7 @@ tap.test('alias.put() - URL parameters is URL encoded', async (t) => {
     t.end();
 });
 
-tap.test('alias.put() - Prevent unexisting alias', async (t) => {
+tap.test('alias.put() - Prevent non-existing package to map to alias', async (t) => {
     const sink = new Sink();
     const h = new Handler({ sink });
 
@@ -51,6 +51,86 @@ tap.test('alias.put() - Prevent unexisting alias', async (t) => {
     const res = h.handler(req, 'anton', 'pkg', '%40foo%2Fbar-lib', '8');
 
     t.rejects(res, HttpError.NotFound)
+
+    t.end();
+});
+
+tap.test('alias.put() - Map alias to existing npm package', async (t) => {
+    const sink = new Sink();
+    sink.set('/local/npm/@bar/baz/17.0.2.package.json', 'payload');
+
+    const h = new Handler({ sink });
+
+    const formData = new FormData();
+    formData.append('version', '17.0.2');
+
+    const headers = formData.getHeaders();
+    const req = new Request({ headers });
+    formData.pipe(req);
+
+    const res = await h.handler(req, 'anton', 'npm', '%40bar%2Fbaz', '8');
+
+    t.equal(res.statusCode, 303, 'should respond with expected status code');
+    t.equal(res.location, '/npm/@bar/baz/v8', '.location should be decoded');
+
+    t.end();
+});
+
+tap.test('alias.put() - Prevent non-existing npm to map to alias', async (t) => {
+    const sink = new Sink();
+
+    const h = new Handler({ sink });
+
+    const formData = new FormData();
+    formData.append('version', '17.0.2');
+
+    const headers = formData.getHeaders();
+    const req = new Request({ headers });
+    formData.pipe(req);
+
+    const res = h.handler(req, 'anton', 'npm', '%40bar%2Fbaz', '8');
+
+    t.rejects(res, HttpError.NotFound);
+
+    t.end();
+});
+
+tap.test('alias.put() - Map alias to existing import-map', async (t) => {
+    const sink = new Sink();
+    sink.set('/local/map/@cuz/fuzz/8.1.4.import-map.json', 'payload');
+
+    const h = new Handler({ sink });
+
+    const formData = new FormData();
+    formData.append('version', '8.1.4');
+
+    const headers = formData.getHeaders();
+    const req = new Request({ headers });
+    formData.pipe(req);
+
+    const res = await h.handler(req, 'anton', 'map', '%40cuz%2Ffuzz', '8');
+
+    t.equal(res.statusCode, 303, 'should respond with expected status code');
+    t.equal(res.location, '/map/@cuz/fuzz/v8', '.location should be decoded');
+
+    t.end();
+});
+
+tap.test('alias.put() - Prevent non-existing import-map to map to alias', async (t) => {
+    const sink = new Sink();
+
+    const h = new Handler({ sink });
+
+    const formData = new FormData();
+    formData.append('version', '8.1.4');
+
+    const headers = formData.getHeaders();
+    const req = new Request({ headers });
+    formData.pipe(req);
+
+    const res = h.handler(req, 'anton', 'map', '%40cuz%2Ffuzz', '8');
+    t.rejects(res, HttpError.NotFound);
+
 
     t.end();
 });
