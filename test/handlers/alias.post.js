a5b960170e4b9bae26b0200184d0dcfc981b7d34
Martin Folkeseth <martin.folkeseth@gmail.com>
2021-10-11 11:59:13 +0200

    fix: prevent unexisting aliases (#276)
    * prevent alias references to packages that does not exist
    
    * Add same check to put handler
    
    * Check existing package file and and add test for alias put and post
    
    * remove console log

--- test/handlers/alias.post.js
+++ test/handlers/alias.post.js
@@ -3,6 +3,7 @@
 const { PassThrough } = require('stream');
 const FormData = require('form-data');
 const tap = require('tap');
+const HttpError = require('http-errors');
 
 const Handler = require('../../lib/handlers/alias.post.js');
 const Sink = require('../../lib/sinks/test');
@@ -19,6 +20,7 @@ const Request = class Request extends PassThrough {
 
 tap.test('alias.post() - URL parameters is URL encoded', async (t) => {
     const sink = new Sink();
+    sink.set('/local/pkg/@foo/bar-lib/8.1.4-1.package.json', 'payload');
     sink.set('/local/pkg/@foo/bar-lib/8.alias.json', 'payload');
 
     const h = new Handler({ sink });
@@ -34,5 +36,25 @@ tap.test('alias.post() - URL parameters is URL encoded', async (t) => {
 
     t.equal(res.statusCode, 303, 'should respond with expected status code');
     t.equal(res.location, '/pkg/@foo/bar-lib/v8', '.location should be decoded');
+    t.end();
+});
+
+tap.test('alias.post() - Prevent unexisting alias', async (t) => {
+    const sink = new Sink();
+    sink.set('/local/pkg/@foo/bar-lib/8.alias.json', 'payload');
+
+    const h = new Handler({ sink });
+
+    const formData = new FormData();
+    formData.append('version', '8.1.4-1');
+
+    const headers = formData.getHeaders();
+    const req = new Request({ headers });
+    formData.pipe(req);
+
+    const res = h.handler(req, 'anton', 'pkg', '%40foo%2Fbar-lib', '8');
+
+    t.rejects(res, HttpError.NotFound);
+
     t.end();
 });
